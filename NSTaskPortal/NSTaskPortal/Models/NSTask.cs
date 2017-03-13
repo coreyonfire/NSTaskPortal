3dd4eac2a9a0d7fd69b35ea0924ffeb6466583b3
coreyb <coreyb@bankerstoolbox.com>
2017-03-13 09:53:22 -0500

    Added NSTask Class. Implemented Support and Project task child classes.

---
+++ NSTaskPortal/NSTaskPortal/Models/NSTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NSTaskPortal.Models
+{
+    public class NSTask
+    {
+        protected string Name { get; set; }
+        protected double Hours { get; set; }
+        protected String AssignedTo { get; set; }
+
+        public NSTask(string n = "NO TITLE PROVIDED", double h = 0.0, string a = "NOT ASSIGNED")
+        {
+            Name = n;
+            Hours = h;
+            AssignedTo = a;
+        }
+    }
+
+    public class NSSupportTask : NSTask
+    {
+        protected int CaseNumber { get; set; }
+
+        public NSSupportTask(string n = "NO TITLE PROVIDED", double h = 0.0, string a = "NOT ASSINGED", int c = 0) 
+            : base(n, h, a)
+        {
+            CaseNumber = c;
+        }
+    }
+
+    public class NSProjectTask : NSTask
+    {
+        // project tasks are the case themselves so do i need to store case number? I will for now, just in case
+        public NSProjectTask(string n = "NO TITLE PROVIDED", double h = 0.0, string a = "NOT ASSINGED") 
+            : base(n, h, a)
+        { }
+    }
+}
